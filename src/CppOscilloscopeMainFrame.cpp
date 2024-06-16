e2b159996eced8c25b2402348463c4778cd3b50d
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2024-06-16 07:28:00 +0200

    Add event handling for the radio boxes and the checkbox

--- src/CppOscilloscopeMainFrame.cpp
+++ src/CppOscilloscopeMainFrame.cpp
@@ -1,48 +1,38 @@
 #include "CppOscilloscopeMainFrame.h"
 
-CppOscilloscopeMainFrame::CppOscilloscopeMainFrame( wxWindow* parent )
-:
-MainFrame( parent )
-{
+CppOscilloscopeMainFrame::CppOscilloscopeMainFrame(wxWindow* parent)
+    : MainFrame(parent), _config{OperatingModeRadioBox->GetSelection(), DrawStyleRadioBox->GetSelection(), ShowGridCheckbox->IsChecked()} {};
 
+void CppOscilloscopeMainFrame::DrawPanelOnPaint(wxPaintEvent& event) {
+  // TODO: Implement DrawPanelOnPaint
 }
 
-void CppOscilloscopeMainFrame::DrawPanelOnPaint( wxPaintEvent& event )
-{
-// TODO: Implement DrawPanelOnPaint
+void CppOscilloscopeMainFrame::DrawPanelOnUpdateUI(wxUpdateUIEvent& event) {
+  // TODO: Implement DrawPanelOnUpdateUI
 }
 
-void CppOscilloscopeMainFrame::DrawPanelOnUpdateUI( wxUpdateUIEvent& event )
-{
-// TODO: Implement DrawPanelOnUpdateUI
+void CppOscilloscopeMainFrame::LoadFileButtonOnButtonClick(wxCommandEvent& event) {
+  // TODO: Implement LoadFileButtonOnButtonClick
 }
 
-void CppOscilloscopeMainFrame::LoadFileButtonOnButtonClick( wxCommandEvent& event )
-{
-// TODO: Implement LoadFileButtonOnButtonClick
+void CppOscilloscopeMainFrame::OperatingModeRadioBoxOnRadioBox(wxCommandEvent& event) {
+  OperatingMode mode = static_cast<OperatingMode>(OperatingModeRadioBox->GetSelection());
+  _config.setOperatingMode(mode);
 }
 
-void CppOscilloscopeMainFrame::OperatingModeRadioBoxOnRadioBox( wxCommandEvent& event )
-{
-// TODO: Implement OperatingModeRadioBoxOnRadioBox
+void CppOscilloscopeMainFrame::DrawStyleRadioBoxOnRadioBox(wxCommandEvent& event) {
+  DrawStyle style = static_cast<DrawStyle>(DrawStyleRadioBox->GetSelection());
+  _config.setDrawStyle(style);
 }
 
-void CppOscilloscopeMainFrame::DrawStyleRadioBoxOnRadioBox( wxCommandEvent& event )
-{
-// TODO: Implement DrawStyleRadioBoxOnRadioBox
+void CppOscilloscopeMainFrame::ShowGridCheckboxOnCheckBox(wxCommandEvent& event) {
+  _config.setShowGrid(ShowGridCheckbox->IsChecked());
 }
 
-void CppOscilloscopeMainFrame::ShowGridCheckboxOnCheckBox( wxCommandEvent& event )
-{
-// TODO: Implement ShowGridCheckboxOnCheckBox
+void CppOscilloscopeMainFrame::BitmapSaveButtonOnButtonClick(wxCommandEvent& event) {
+  // TODO: Implement BitmapSaveButtonOnButtonClick
 }
 
-void CppOscilloscopeMainFrame::BitmapSaveButtonOnButtonClick( wxCommandEvent& event )
-{
-// TODO: Implement BitmapSaveButtonOnButtonClick
-}
-
-void CppOscilloscopeMainFrame::RefreshTimerOnTimer( wxTimerEvent& event )
-{
-// TODO: Implement RefreshTimerOnTimer
+void CppOscilloscopeMainFrame::RefreshTimerOnTimer(wxTimerEvent& event) {
+  // TODO: Implement RefreshTimerOnTimer
 }
