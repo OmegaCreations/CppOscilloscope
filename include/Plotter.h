41be2e8c3a359bf47734457316035b1b4c967068
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2024-06-17 20:37:33 +0200

    Add y params to the drawAxes method back and remove unnecessary params from line2d

--- include/Plotter.h
+++ include/Plotter.h
@@ -11,12 +11,12 @@ class Plotter {
  private:
   std::shared_ptr<Config> _config;
 
-  void line2d(wxDC& dc, const Matrix& transformation, std::pair<double, double> p1, std::pair<double, double> p2, int move_x1 = 0, int move_x2 = 0);
+  void line2d(wxDC& dc, const Matrix& transformation, std::pair<double, double> p1, std::pair<double, double> p2);
 
   public:
   Plotter(std::shared_ptr<Config> config);
   void draw(wxDC& dc, const Data& currentData, const Data& previousData, const Data& historicData);
-  void drawAxes(wxDC& dc, const Data& currentData, double x0, double x1);
+  void drawAxes(wxDC& dc, const Data& currentData, double x0, double x1, double y0, double y1);
 };
 
 #endif /* PLOTTER_H */
